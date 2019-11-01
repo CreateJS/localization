e6bda0765477a8f077c702aee121eb2c81c28e1e
“名yyasunao <54300465+YasunaoYamashita@users.noreply.github.com>
2019-11-01 19:43:31 +0900

    trtrtrtr

--- ja/SoundJS/0.4.0/src/soundjs/errors/errors_ja-jp.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	var logs = {
-		AUDIO_FAILED_404: "ファイル %DETAILS が再生できません。404 ファイルが見つかりませんでした。",
-		AUDIO_FAILED_INTERRUPT: "ファイル %DETAILS% が再生できません。割り込みに利用可能なチャネルがありません。",
-		AUDIO_FAILED_NOT_LOADED: "ファイル %DETAILS% が再生できません。まだ読み込みが未完了です。",
-		AUDIO_FLASH_FAILED: "Flash を初期化することができませんでした。"
-	}
-	createjs.Log && createjs.Log.addKeys(logs);
-}())
